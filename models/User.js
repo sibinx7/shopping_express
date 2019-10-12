0fbde19483c5f4897aa95fb9e381ee0125c18157
Sibin Xavier <sibinx7@gmail.com>
2019-10-12 11:47:27 +0530

    Working progress with user api

--- models/User.js
+++ models/User.js
@@ -1,7 +1,29 @@
 const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
-	username: String,
-	password: String
+	email: {
+		type: String,
+		required: true,
+		unique: true 
+	},
+	first_name: String,
+	last_name: String,
+	username: {
+		type: String,
+		unique: true,
+		required: true 
+	},
+	password: {
+		type: String,
+		required: true 
+	},
+	address: String,
+	country: String,
+	roles: Array,
+	dob: Date,	
+	image: String, 
+	last_active: Date,
+	created_at: Date,	
+	updated_at: Date   
 });
 
 const User = module.exports =mongoose.model('User', UserSchema);
