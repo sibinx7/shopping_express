034b8a4b8c54df87e6712c60a3aec291a83b4e15
Sibin Xavier <sibinx7@gmail.com>
2019-10-20 14:04:19 +0530

    Add super and admin accounts

--- seed/index.js
+++ seed/index.js
@@ -4,31 +4,45 @@ const User = require("../models/User");
  * @description Create a super admin if no super admin user present
  */
 User.countDocuments({super_admin: true}, (err,count) => {
-
-	if(!err && count === 0){
+	if(!err){
 		// Create a User
-		console.log("Exisitng admins", count)
 		User.deleteOne({email: "admin@admin.com"}, (err) => {
 			if(!err){
 				User.create({
-					email: "admin@admin.com",
-					username: "admin@admin.com",
+					email: "super_admin@admin.com",
+					username: "super_admin@admin.com",
 					password: "123456",
-					roles:["admin", "super_admin"],
+					roles:["super_admin"],
 					created_at: new Date(),
 					updated_at: new Date(),
-					first_name: "admin",
+					first_name: "super",
+					last_name: "admin",
 					gender: "Male",
-					super_admin: true
+					super_admin: true,
+					admin: false
 				}, (err, user) => {
-					console.log(JSON.stringify(err))
 					if(!err){
-						console.log("User created")
-						console.log(JSON.stringify(user))
 					}
 				})
 			}
 		})
+	}
+});
 
+
+User.countDocuments({admin: true}, (err, count) => {
+	if(!err && count === 0){
+		User.create({
+			email: "admin@admin.com",
+			username: "admin@admin.com",
+			password: "123456",
+			roles:["admin"],
+			created_at: new Date(),
+			updated_at: new Date(),
+			first_name: "admin",
+			gender:"Male",
+			super_admin: false,
+			admin: true,
+		});
 	}
 });
