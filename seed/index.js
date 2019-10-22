45ee28c2c5559a3057c36afe2fdd4919d38f61bb
Sibin Xavier <sibinx7@gmail.com>
2019-10-22 22:12:45 +0530

    Change password to base 64

--- seed/index.js
+++ seed/index.js
@@ -27,15 +27,26 @@ User.countDocuments({super_admin: true}, (err,count) => {
 			}
 		})
 	}
+	console.log("Updating password...")
+
+	User.updateOne({email:"super_admin@admin.com"},{
+		password: Base64.encode("123456"),
+		updated_at: new Date()
+	}, (err, user) => {
+		console.log(JSON.stringify(err))
+		console.log(JSON.stringify(user))
+		console.log("User update one....")
+	})
 });
 
 
 User.countDocuments({admin: true}, (err, count) => {
+	console.log("Admin user count", count)
 	if(!err && count === 0){
 		User.create({
 			email: "admin@admin.com",
 			username: "admin@admin.com",
-			password: "123456",
+			password: Base64.encode("123456"),
 			roles:["admin"],
 			created_at: new Date(),
 			updated_at: new Date(),
@@ -45,4 +56,12 @@ User.countDocuments({admin: true}, (err, count) => {
 			admin: true,
 		});
 	}
+
+	User.updateOne({
+		email:"admin@admin.com"
+	}, {
+		password: Base64.encode("123456"),
+		updated_at: new Date()
+	});
+
 });
