a9499babb1e3a74f353f0333325796bb972f2715
Sibin Xavier <sibinx7@gmail.com>
2019-10-22 23:47:52 +0530

    Update user section

--- seed/index.js
+++ seed/index.js
@@ -1,4 +1,6 @@
+import {Base64} from "js-base64";
 const User = require("../models/User");
+
 
 /**
  * @description Create a super admin if no super admin user present
@@ -6,41 +8,48 @@ const User = require("../models/User");
 User.countDocuments({super_admin: true}, (err,count) => {
 	if(!err){
 		// Create a User
-		User.deleteOne({email: "admin@admin.com"}, (err) => {
-			if(!err){
-				User.create({
-					email: "super_admin@admin.com",
-					username: "super_admin@admin.com",
-					password: "123456",
-					roles:["super_admin"],
-					created_at: new Date(),
-					updated_at: new Date(),
-					first_name: "super",
-					last_name: "admin",
-					gender: "Male",
-					super_admin: true,
-					admin: false
-				}, (err, user) => {
-					if(!err){
-					}
-				})
-			}
-		})
+		if(count === 0){
+			User.create({
+				email: "super_admin@admin.com",
+				username: "super_admin@admin.com",
+				password: Base64.encode("123456"),
+				roles:["super_admin"],
+				created_at: new Date(),
+				updated_at: new Date(),
+				first_name: "super",
+				last_name: "admin",
+				gender: "Male",
+				super_admin: true,
+				admin: false,
+				active: true
+			}, (err, user) => {
+				if(!err){
+				}
+			});
+		}else{
+			console.log("Updating super admin password...");
+			console.log(Base64.encode("123456"))
+			User.updateOne({email:"super_admin@admin.com"},{
+				password: Base64.encode("123456"),
+				updated_at: new Date(),
+				active: true
+			}, (err, user) => {
+				console.log(JSON.stringify(err))
+				console.log(JSON.stringify(user))
+				console.log("Super admin update one....")
+			})
+		}
+		// User.deleteOne({email: "admin@admin.com"}, (err) => {
+		// 	if(!err){
+		//
+		// 	}
+		// })
 	}
-	console.log("Updating password...")
 
-	User.updateOne({email:"super_admin@admin.com"},{
-		password: Base64.encode("123456"),
-		updated_at: new Date()
-	}, (err, user) => {
-		console.log(JSON.stringify(err))
-		console.log(JSON.stringify(user))
-		console.log("User update one....")
-	})
 });
 
 
-User.countDocuments({admin: true}, (err, count) => {
+User.countDocuments({email: "admin@admin.com"}, (err, count) => {
 	console.log("Admin user count", count)
 	if(!err && count === 0){
 		User.create({
@@ -54,14 +63,21 @@ User.countDocuments({admin: true}, (err, count) => {
 			gender:"Male",
 			super_admin: false,
 			admin: true,
+			active: true
+		});
+	}else{
+		console.log("Admin user update soon..")
+		console.log(Base64.encode("123456"))
+		User.updateOne({
+			email:"admin@admin.com"
+		}, {
+			password: Base64.encode("123456"),
+			updated_at: new Date(),
+			active: true
+		}, (err, user) => {
+			console.log(JSON.stringify(err))
+			console.log(JSON.stringify(user))
+			console.log("Admin user updated...")
 		});
 	}
-
-	User.updateOne({
-		email:"admin@admin.com"
-	}, {
-		password: Base64.encode("123456"),
-		updated_at: new Date()
-	});
-
 });
