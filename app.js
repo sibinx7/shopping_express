ace4681b3f8e91587aaaa828176c6b2f3ac951ee
Sibin Xavier <sibinx7@gmail.com>
2019-10-30 15:59:49 +0530

    Fix CSRF issues

--- app.js
+++ app.js
@@ -59,13 +59,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
+app.use("/api",  usersAPIRouter); // csrf token exception
 
 
 app.use(cors());
 app.options('*', cors());
-
-app.use("/api",  usersAPIRouter); // csrf token exception
-
 
 app.use(cookieParser());
 
